edb7b1759231294e2664b992bc66336398a3f700
unknown <pybarguen@gmail.com>
2025-07-20 17:25:45 -0500

    Feat : Se ha realizado el código base para mostrar la interfaz hecha en QTDesigner en el IDE de Pycharm.

---
+++ Python_Interface/Interface.py
@@ -0,0 +1,16 @@
+import sys
+import PyQt5
+import serial
+from Desing import *
+class Program(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+
+    app = QtWidgets.QApplication(sys.argv)
+    mi_app = Program()
+    mi_app.show()
+    sys.exit(app.exec_())
