eeeac15a2d77b5fb0c6e3343ebbe8d21d62054c3
unknown <pybarguen@gmail.com>
2025-08-03 20:46:21 -0500

    feat : Se ha añadido funcionalidades en la interfaz para la selección del puerto y la velocidad de comunicación entre el hardware de lectura y escritura de la memoria

--- Python_Interface/Interface.py
+++ Python_Interface/Interface.py
@@ -5,7 +5,8 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QFileDialog
 from PIL import Image
 import numpy as np
-from pycparser.c_ast import Break
+
+
 
 from Desing import *
 
@@ -15,8 +16,18 @@ class Program(QtWidgets.QMainWindow):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-
+        ports = ["Com1", "Com2", "Com3", "Com4"]
+        baudrates = [
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "230400",
+            "460800"
+        ]
+        self.ui.ComboBox_Port.addItems(ports)
+        self.ui.ComboBox_BaudRate.addItems(baudrates)
         #Connect Import Image Button With the respective Funtion
         self.ui.Import_Image_button.clicked.connect(self.Import_Images)
 
@@ -38,19 +49,23 @@ class Program(QtWidgets.QMainWindow):
 
     def Pixel_Converter(self, Pixels):
         alto, ancho, _ = Pixels.shape
+        Image_matrix = []
         for i in range(0, alto):
             for j in range(0, ancho):
                 Red = int(Pixels[i, j, 0])
                 Green = int(Pixels[i, j, 1])
                 Blue = int(Pixels[i, j, 2])
-                r_5_bits = (Red*31)/255
-                g_6_bits = (Green*63)/255
-                b_5_bits = (Blue*31)/255
+                r_5_bits = (Red >> 3) & 0x1F
+                g_6_bits = (Green >> 2) & 0x3F
+                b_5_bits = (Blue >> 3) & 0x1F
 
                 print(Red, Green, Blue)
-                print(r_5_bits, g_6_bits, b_5_bits)
-                break
-            break
+                print("Red {} Green {}  Blue {}".format(r_5_bits, g_6_bits, b_5_bits))
+                print("Red {} Green {}  Blue {}".format(bin(r_5_bits), bin(g_6_bits), bin(b_5_bits)))
+                Final_value = (r_5_bits << 11) | (g_6_bits << 5) | b_5_bits
+                Image_matrix.append(hex(Final_value))
+        print(Image_matrix)
+
 
 
 
