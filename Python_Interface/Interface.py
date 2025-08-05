e698502bb6f3f4910881f97b1699a180517b2e11
unknown <pybarguen@gmail.com>
2025-08-04 20:31:08 -0500

    Feat : Se ha añadido mas funcionalidades a la interfaz para el inicio de la comunicación con el Hardware

--- Python_Interface/Interface.py
+++ Python_Interface/Interface.py
@@ -4,6 +4,7 @@ import serial
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QFileDialog
 from PIL import Image
+from Serial_server import *
 import numpy as np
 
 
@@ -13,10 +14,25 @@ from Desing import *
 
 class Program(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
+
+
+
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        ports = ["Com1", "Com2", "Com3", "Com4"]
+
+        self.Serial_data = None
+
+
+
+        """
+        Configuracion de los WidGets ComboBox
+        
+        """
+        ports = ["COM1", "COM2", "COM3", "COM4"]
+
+        self.ui.ComboBox_Port.addItems(ports)
+
         baudrates = [
             "9600",
             "19200",
@@ -26,10 +42,40 @@ class Program(QtWidgets.QMainWindow):
             "230400",
             "460800"
         ]
-        self.ui.ComboBox_Port.addItems(ports)
+
         self.ui.ComboBox_BaudRate.addItems(baudrates)
+
+        """
+                   ______________________________________________________________
+
+                   """
+
+
+
+
+
+        """
+        Configuraciones de los botones con sus respectivas funciones 
+        
+        """
+
+
+
         #Connect Import Image Button With the respective Funtion
         self.ui.Import_Image_button.clicked.connect(self.Import_Images)
+        self.ui.Connect_Serial.clicked.connect(
+            lambda: self.Connect_Serial(
+                self.ui.ComboBox_Port.currentText(),
+                self.ui.ComboBox_BaudRate.currentText()
+            )
+        )
+
+    """
+            ______________________________________________________________
+
+            """
+
+
 
 
     def Import_Images(self):
@@ -65,6 +111,12 @@ class Program(QtWidgets.QMainWindow):
                 Final_value = (r_5_bits << 11) | (g_6_bits << 5) | b_5_bits
                 Image_matrix.append(hex(Final_value))
         print(Image_matrix)
+
+
+
+    def Connect_Serial(self, port, baudrate):
+        print("Puerto {}   Baudrate {}".format(port, baudrate))
+        #self.Serial_data = Serial_data(port, baudrate)
 
 
 
